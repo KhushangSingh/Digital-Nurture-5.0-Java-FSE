27d20a3b0c974f659a5f370279156ff0bed961fb
Khushang Singh <147972452+KhushangSingh@users.noreply.github.com>
2026-06-29 00:33:35 +0530

    Week 2 --> Spring Core and Maven Exercises

---
+++ Week 2/Spring Core and Maven/LibraryManagement-Exercise4/src/main/java/com/library/app/LibraryManagementApplication.java
@@ -0,0 +1,8 @@
+package com.library.app;
+
+public class LibraryManagementApplication {
+
+    public static void main(String[] args) {
+        System.out.println("LibraryManagement Exercise 4 project is ready.");
+    }
+}
