9036b47b7e14ffe967605a0cf022ef73597d9212
Khushang Singh <147972452+KhushangSingh@users.noreply.github.com>
2026-07-01 23:18:16 +0530

    Spring Core and Maven --> Exercise 4

--- Week 2/Spring Core and Maven/LibraryManagement-Exercise4/src/main/java/com/library/app/LibraryManagementApplication.java
+++ Week 2/Spring Core and Maven/LibraryManagement-Exercise4/src/main/java/com/library/app/LibraryManagementApplication.java
@@ -1,8 +1,14 @@
 package com.library.app;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.library.service.BookService;
+
 public class LibraryManagementApplication {
 
     public static void main(String[] args) {
-        System.out.println("LibraryManagement Exercise 4 project is ready.");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService bookService = context.getBean("bookService", BookService.class);
+        bookService.showBook();
+        context.close();
     }
 }
